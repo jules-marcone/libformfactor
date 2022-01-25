ff07da59edbc23e54ccde3de8ab8b90ea4cede67
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2022-01-25 17:48:31 +0100

    demo: octahedral formfactor for three different q directions

--- demo/octahedron.cpp
+++ demo/octahedron.cpp
@@ -19,10 +19,27 @@
 
 int main()
 {
+    std::cout << "# Octahedral form factor, for different q scans\n";
     ff::platonic::Octahedron octahedron(1.);
+
+    std::cout << "# q vs |F(q)| for q in direction 111, perpendicular to two faces\n";
     for (double t = 0.2; t < 200; t *= 1.002) {
-        // choose q perpendicular to two opposite faces
-        C3 q(0, sqrt(2. / 3) * t, sqrt(1. / 3) * t);
+        C3 q(t/sqrt(3), t/sqrt(3), t/sqrt(3));
         std::cout << t << " " << std::abs(octahedron.formfactor(q)) << std::endl;
     }
+    std::cout << std::endl;
+
+    std::cout << "# q vs |F(q)| for q in direction 110, perpendicular to two edges\n";
+    for (double t = 0.2; t < 200; t *= 1.002) {
+        C3 q(t/sqrt(2), t/sqrt(2), 0);
+        std::cout << t << " " << std::abs(octahedron.formfactor(q)) << std::endl;
+    }
+    std::cout << std::endl;
+
+    std::cout << "# q vs |F(q)| for q in direction 345, no special symmetry\n";
+    for (double t = 0.2; t < 200; t *= 1.002) {
+        C3 q(3*t/sqrt(50), 4*t/sqrt(50), 5*t/sqrt(50));
+        std::cout << t << " " << std::abs(octahedron.formfactor(q)) << std::endl;
+    }
+    std::cout << std::endl;
 }
