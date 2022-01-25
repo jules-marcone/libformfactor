fecd79b347b2a622fb308532d5eabdb09351205f
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2022-01-25 18:40:47 +0100

    demo/octahedron: two scans along grand cercles

--- demo/octahedron.cpp
+++ demo/octahedron.cpp
@@ -15,6 +15,8 @@
 #include <ff/Platonic.h>
 #include <iostream>
 
+constexpr double twopi = std::atan(1)*8;
+
 //! Prints list t vs |F(q(t))| for a logarithmic range of t values
 
 int main()
@@ -39,6 +41,24 @@ int main()
     std::cout << "# q vs |F(q)| for q in direction 345, no special symmetry\n";
     for (double t = 0.2; t < 200; t *= 1.002) {
         C3 q(3*t/sqrt(50), 4*t/sqrt(50), 5*t/sqrt(50));
+        std::cout << t << " " << std::abs(octahedron.formfactor(q)) << std::endl;
+    }
+    std::cout << std::endl;
+
+    std::cout << "# q vs |F(q)| for |q|=50, q on grand cercle through 111 and -1,-1,1 directions\n";
+    const C3 a1(1/sqrt(3), 1/sqrt(3), 1/sqrt(3));
+    const C3 a2(-1/sqrt(6), -1/sqrt(6), 2/sqrt(6));
+    for (double t = 0; t < twopi; t += twopi/500) {
+        C3 q = 50.*(cos(t)*a1+sin(t)*a2);
+        std::cout << t << " " << std::abs(octahedron.formfactor(q)) << std::endl;
+    }
+    std::cout << std::endl;
+
+    std::cout << "# q vs |F(q)| for |q|=50, q on grand cercle through 111 and -2,3,-5 directions\n";
+    const C3 b1(1/sqrt(3), 1/sqrt(3), 1/sqrt(3));
+    const C3 b2(-8/sqrt(98), 3/sqrt(98), 5/sqrt(98));
+    for (double t = 0; t < twopi; t += twopi/500) {
+        C3 q = 50.*(cos(t)*b1+sin(t)*b2);
         std::cout << t << " " << std::abs(octahedron.formfactor(q)) << std::endl;
     }
     std::cout << std::endl;
